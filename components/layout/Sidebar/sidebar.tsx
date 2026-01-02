a041d0b68090151c73b3b76521a6dffb427e97f8
Matheus da Silva <silva.contadev@gmail.com>
2026-01-02 13:01:21 -0300

    :sparkles: feat(sidebar) sidebar completed - Routes defined - Standardized styling defined using the isActive function

---
+++ components/layout/Sidebar/sidebar.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import {
+    Wallet,
+    LayoutDashboard,
+    Landmark,
+    Tags,
+    ArrowRightLeft,
+} from "lucide-react";
+import styles from "./Sidebar.module.scss";
+
+export function Sidebar() {
+    const pathname = usePathname();
+
+    const navItems = [
+        {
+            name: "Resumo",
+            icon: LayoutDashboard,
+            path: "/",
+        },
+        {
+            name: "Contas",
+            icon: Landmark,
+            path: "/contas",
+        },
+        {
+            name: "Transações",
+            icon: ArrowRightLeft,
+            path: "/transacoes",
+        },
+        {
+            name: "Categorias",
+            icon: Tags,
+            path: "/categorias",
+        },
+    ];
+
+    return (
+        <aside className={styles.aside}>
+            <header className={styles.logo}>
+                <div className={styles.logoIcon}>
+                    <Wallet />
+                </div>
+                <span>FinControl</span>
+            </header>
+            <nav className={styles.navbar}>
+                <ul>
+                    {navItems.map((item) => {
+                        const isActive = pathname === item.path;
+
+                        return (
+                            <li key={item.name}>
+                                <a
+                                    className={
+                                        isActive
+                                            ? styles.navItemActive
+                                            : styles.navItem
+                                    }
+                                    href={item.path}
+                                >
+                                    <item.icon className={styles.navIcon} />
+                                    <span>{item.name}</span>
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
+
+            <footer>
+                    &copy; 2026 FinControl
+            </footer>
+        </aside>
+    );
+}
